d01b068e7b9845f6936d7502ef216628fb6454a6
rnishio <quick.xp@me.com>
2015-02-18 00:47:09 +0900

    nginx first commit

---
+++ site-cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,63 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2015, r_nishio
+#
+package "nginx" do
+	action :install
+end
+
+service 'nginx' do
+	supports :status => true, :restart => true, :reload => true
+	action   [:enable, :start]
+end
+
+################
+#Create Directory
+################
+#Base Directory
+directory node['nginx']['dir'] do
+	owner     'root'
+	group     'root'
+	mode      '0755'
+	recursive true
+end
+
+#site directory
+%w(sites-available sites-enabled conf.d).each do |leaf|
+  directory File.join(node['nginx']['dir'], leaf) do
+    owner 'root'
+    group node['root_group']
+    mode  '0755'
+  end
+end
+
+#Log Directory
+directory node['nginx']['log_dir'] do
+	mode      '0755'
+	owner     node['nginx']['user']
+	action    :create
+	recursive true
+end
+
+##################
+#Config File
+##################
+
+template 'nginx.conf' do
+  path   "#{node['nginx']['dir']}/nginx.conf"
+  source 'nginx.conf.erb'
+  owner  'root'
+  group  'root'
+  mode   '0644'
+  notifies :reload, 'service[nginx]'
+end
+
+template "#{node['nginx']['dir']}/sites-available/default" do
+  source 'default-site.erb'
+  owner  'root'
+  group  'root'
+  mode   '0644'
+  notifies :reload, 'service[nginx]'
+end
