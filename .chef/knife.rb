68e1805f44a292b340f06b37494af3691d5f1458
rnishio <quick.xp@me.com>
2015-02-23 22:41:27 +0900

    eopes_local first

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,8 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-knife[:berkshelf_path] = "cookbooks"
+log_level                :info
+log_location             STDOUT
+node_name                'r_nishio'
+client_key               '/Users/r_nishio/000_work/EOPES_ENV/.chef/r_nishio.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://rnmac.local:443'
+syntax_check_cache_path  '/Users/r_nishio/000_work/EOPES_ENV/.chef/syntax_check_cache'
