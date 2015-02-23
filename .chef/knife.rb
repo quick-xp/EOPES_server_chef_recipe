1bba9d7e4474b066dcb3d80e87a3b0fa2bdaf381
rnishio <quick.xp@me.com>
2015-02-23 23:20:48 +0900

    gem update

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,3 +6,7 @@ validation_client_name   'chef-validator'
 validation_key           '/etc/chef-server/chef-validator.pem'
 chef_server_url          'https://rnmac.local:443'
 syntax_check_cache_path  '/Users/r_nishio/000_work/EOPES_ENV/.chef/syntax_check_cache'
+
+role_path './roles'
+cookbook_path './site-cookbooks'
+knife[:solo_path] = '/tmp/chef-solo'
--- Berksfile
+++ Berksfile
@@ -1,5 +1,5 @@
 site :opscode
 cookbook 'git'
-cookbook 'rvm', git: 'https://github.com/fnichol/chef-rvm.git'
+cookbook 'rvm', git: 'https://github.com/fnichol/chef-rvm.git', :ref => '24ecbb0'
 cookbook 'mysql'
 
